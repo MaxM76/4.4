6955ec82cda6fd48970337b5370be4ef9017cc59
MaxM76 <38570363+MaxM76@users.noreply.github.com>
2018-08-11 17:50:44 +0300

    Add files via upload

--- manage_db.php
+++ manage_db.php
@@ -1,9 +1,22 @@
 <?php
 
-$action = 'show';
-
+if (isset($_GET['current_table'])) {
+    $currentTable = (string) $_GET['current_table'];
+} else {
+    $currentTable = '';
+}
 
 require_once __DIR__ . '/connect_db.php';
+
+$query = 'SHOW TABLES';
+$cat = $pdo->query($query);
+
+try {
+    $dbTables = $cat->fetchAll(PDO::FETCH_NUM);
+} catch (PDOException $e) {
+    echo 'Ошибка выполнения запроса: ' . $e->getMessage();
+    echo '<br>';
+}
 
 if (isset($_GET['action'])) {
     $action = (string) $_GET['action'];
@@ -13,87 +26,100 @@ if (isset($_GET['action'])) {
 
 switch ($action) {
     case 'table_info':
+        echo 'table_info';
+        echo '<br>';
+        echo '$currentTable = ' . $currentTable;
+        echo '<br>';
         if (isset($_GET['selected_table'])) {
-            $nameSelectedTable = (string) $_GET['selected_table'];
-            try {
-                $query = "SHOW COLUMNS FROM books";
+            $currentTable = (string)$_GET['selected_table'];
+        }
+         /*   try {
+                $query = "SHOW COLUMNS FROM $currentTable";
 
                 $cat = $pdo->prepare($query);
-                $cat->execute([':nameSelectedTable' => $nameSelectedTable]);
+                $cat->execute();
                 $tableFields = $cat->fetchAll(PDO::FETCH_ASSOC);
-                var_dump($tableFields);
-                echo '<pre>';
-                print_r($tableFields);
-                echo '</pre>';
+
             } catch (PDOException $e) {
                 echo 'Ошибка выполнения запроса: ' . $e->getMessage();
+                echo '<br>';
             }
         } else {
             $action = 'show';
-        }
+        }*/
         break;
     case 'create_table':
+        echo 'create_table';
+        echo '<br>';
+        echo '$currentTable = ' . $currentTable;
+        echo '<br>';
         if (isset($_GET['table_name'])) {
             $nameNewTable = (string) $_GET['table_name'];
             try {
-                $query =  "CREATE TABLE new (`id` INT NOT NULL AUTO_INCREMENT,
+                $query =  "CREATE TABLE $nameNewTable (`id` INT NOT NULL AUTO_INCREMENT,
                            PRIMARY KEY(`id`))
                             ENGINE=InnoDB, DEFAULT CHARACTER SET=utf8";
                 $cat = $pdo->prepare($query);
                 $cat->execute([':tableName' => $nameNewTable]);
+                $currentTable = $nameNewTable;
             } catch (PDOException $e) {
                 echo 'Ошибка выполнения запроса: ' . $e->getMessage();
+                echo '<br>';
             }
-        } else {
-            $action = 'show';
         }
         break;
     case 'add_field':
-        if (isset($_GET['field_name'])) {
+        echo 'add_field';
+        echo '<br>';
+        echo '$currentTable = ' . $currentTable;
+        echo '<br>';
+        if (isset($_GET['field_name']) && ($currentTable != '')) {
             $nameNewField = (string) $_GET['field_name'];
-            $typeNewField = (string) $_GET['data_type'];
-            switch ($typeNewField) {
+            switch ((string) $_GET['data_type']) {
                 case 'int' :
-                    $sqlDataType = 'INT';
+                    $typeNewField = 'INT';
                     break;
                 case 'float' :
-                    $sqlDataType = 'FLOAT';
+                    $typeNewField = 'FLOAT';
                     break;
                 case 'timestamp' :
-                    $sqlDataType = 'TIMESTAMP';
+                    $typeNewField = 'TIMESTAMP';
                     break;
                 case 'text' :
-                    $sqlDataType = 'TEXT';
+                    $typeNewField = 'TEXT';
                     break;
             }
 
             try {
-                $query = "ALTER TABLE :nameSelectedTable ADD :nameNewField :typeNewField";
+                $query = "ALTER TABLE $currentTable ADD :nameNewField $typeNewField";
+                // ALTER TABLE `new` ADD `name` INT NOT NULL AFTER `id`;
 
                 $cat = $pdo->prepare($query);
-                $cat->execute([':nameSelectedTable' => $nameSelectedTable, ':nameNewField' => $typeNewField]);
+                $cat->execute([':nameNewField' => $nameNewField]);
 
             } catch (PDOException $e) {
                 echo 'Ошибка выполнения запроса: ' . $e->getMessage();
+                echo '<br>';
             }
-        } else {
-            $action = 'show';
         }
         break;
     case 'delete_field':
-        if (isset($_GET['selected_field'])) {
+        echo 'delete_field';
+        echo '<br>';
+        echo '$currentTable = ' . $currentTable;
+        echo '<br>';
+        if (isset($_GET['selected_field']) && ($currentTable != '')) {
             $nameSelectedField = (string) $_GET['selected_field'];
             try {
-                $query = "ALTER TABLE :nameSelectedTable DROP COLUMN :nameSelectedField";
+                $query = "ALTER TABLE $currentTable DROP COLUMN :nameSelectedField";
 
                 $cat = $pdo->prepare($query);
-                $cat->execute([':nameSelectedTable' => $nameSelectedTable, ':nameSelectedField' => $nameSelectedField]);
+                $cat->execute([':nameSelectedField' => $nameSelectedField]);
 
             } catch (PDOException $e) {
                 echo 'Ошибка выполнения запроса: ' . $e->getMessage();
+                echo '<br>';
             }
-        } else {
-            $action = 'show';
         }
         break;
 
@@ -102,18 +128,24 @@ switch ($action) {
         break;
 }
 
+if ($currentTable != '') {
+    try {
+        $query = "SHOW COLUMNS FROM $currentTable";
 
+        $cat = $pdo->prepare($query);
+        $cat->execute();
+        $tableFields = $cat->fetchAll(PDO::FETCH_ASSOC);
 
-$query =  'SHOW TABLES';
-$cat = $pdo->query($query);
-
-try {
-    $dbTables = $cat->fetchAll(PDO::FETCH_NUM);
-} catch (PDOException $e) {
-    echo 'Ошибка выполнения запроса: ' . $e->getMessage();
+    } catch (PDOException $e) {
+        echo 'Ошибка выполнения запроса: ' . $e->getMessage();
+        echo '<br>';
+    }
 }
 
-
+echo '///////////////////////////////////////////////';
+echo '<br>';
+echo '$currentTable = ' . $currentTable;
+echo '<br>';
 ?>
 
 <!DOCTYPE html>
@@ -125,7 +157,7 @@ try {
   </head>
 
   <body>
-    <p>База данных <?= $nameDB ?></p>
+    <h1>База данных <?= $nameDB ?></h1>
 
     <form method="GET">
       <fieldset>
@@ -136,21 +168,24 @@ try {
       </fieldset>
 
       <fieldset>
-        <legend>Таблицы базы данных</legend>
+        <legend>Таблицы базы данных <?= $nameDB ?></legend>
         <select name="selected_table" size="<?= count($dbTables) ?>">
-        <?php
-        foreach ($dbTables as $dbTable) : ?>
+<?php
+foreach ($dbTables as $dbTable) : ?>
           <option value="<?=$dbTable[0]?>"><?=$dbTable[0]?></option>
-        <?php endforeach;?>
+<?php
+endforeach;
+?>
         </select>
-
+        <input type="hidden" name="current_table" value="<?= $currentTable ?>">
+        <br>
         <label>Выберите таблицу</label>
         <button name="action" value="table_info">Показать информацию о таблице</button>
       </fieldset>
 
-      <fieldset>
-        <legend>Структура таблицы <?= $nameSelectedTable ?></legend>
-        <table>
+      <fieldset <?= ($currentTable =='')?'hidden':'' ?>>
+        <legend>Структура таблицы <?= $currentTable ?></legend>
+        <table border="1">
           <tr>
             <th>Field</th>
             <th>Type</th>
@@ -159,20 +194,25 @@ try {
             <th>Default</th>
             <th>Extra</th>
           </tr>
-          <?php
-          foreach ($tableFields as $tableField) : ?>
+<?php
+if ($currentTable !='') {
+    foreach ($tableFields as $tableField) : ?>
           <tr>
-          <?php
-          foreach ($tableField as $fieldCharacteristic) : ?>
+<?php
+        foreach ($tableField as $fieldCharacteristic) : ?>
             <td><?=$fieldCharacteristic?></td>
-          <?php endforeach;?>
+<?php
+        endforeach;
+?>
           </tr>
-          <?php endforeach;?>
+<?php
+    endforeach;
+}
+?>
         </table>
       </fieldset>
 
-
-      <fieldset>
+      <fieldset <?= ($currentTable =='')?'hidden':'' ?>>
         <legend>Добавление поля</legend>
         <label>Введите имя поля: </label>
         <input type="text" name="field_name" placeholder="Имя поля" value=""><br>
@@ -187,15 +227,28 @@ try {
         <button name="action" value="add_field">Добавить поле</button>
 
       </fieldset>
-      <fieldset>
-        <legend>Удаление поля</legend>
+      <fieldset <?= ($currentTable =='')?'hidden':'' ?>>
+        <legend>Изменение поля</legend>
+        <label>Выберите поле: </label>
         <select name="selected_field" size="<?= count($tableFields) ?>">
-          <?php
-          foreach ($tableFields as $tableField) : ?>
+<?php
+foreach ($tableFields as $tableField) : ?>
           <option value="<?=$tableField['Field']?>"><?=$tableField['Field']?></option>
-          <?php endforeach;?>
+<?php endforeach;?>
         </select>
-        <!-- <input type="submit" name="delete_field" value="Удалить поле"> -->
+
+        <label>Введите новое имя поля: </label>
+        <input type="text" name="field_name" placeholder="Новое имя" value=""><br>
+        <label>Выберите тип данных поля</label>
+        <select name="data_type">
+          <option value="int">INT</option>
+          <option value="float">FLOAT</option>
+          <option value="timestamp">TIMESTAMP</option>
+          <option value="text">TEXT</option>
+        </select>
+                  
+        <br>
+        <button name="action" value="change_field">Изменить</button>
         <button name="action" value="delete_field">Удалить</button>
       </fieldset>
 
